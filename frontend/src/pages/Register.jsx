760c34743ec28f18f849e46a7d38698c661ad28c
Arman shekh <mdarmanshekh4@gmail.com>
2024-07-22 22:53:55 +0530

    adding backend uri in api

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -24,7 +24,7 @@ const Register = () => {
     try {
       await axios
         .post(
-          "http://localhost:4000/api/user/patient/register",
+          "https://hospital-management-backend-nu.vercel.app/api/user/patient/register",
           { firstName, lastName, email, phone, nic, dob, gender, password,role:"Patient" },
           {
             withCredentials: true,
